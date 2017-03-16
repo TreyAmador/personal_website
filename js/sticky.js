87b8208a86e5cb4d9da1d7dde1ddc60b06931612
Trey Amador <tmamador@cpp.edu>
2017-03-16 06:16:34 -0700

    revise sticky function

--- js/sticky.js
+++ js/sticky.js
@@ -3,31 +3,28 @@
 */
 
 
-$(document).ready(function(){
-    $(function() {
-        var nav = $('.nav');
-        var navHomeY = nav.offset().top;
-        var isFixed = false;
-        var $w = $(document);
-        $w.scroll(function() {
-            var scrollTop = $w.scrollTop();
-            var shouldBeFixed = scrollTop > navHomeY;
-            if (shouldBeFixed && !isFixed) {
-                nav.css({
-                    position: 'fixed',
-                    top: 0,
-                    left: nav.offset().left,
-                    width: nav.width()
-                });
-                isFixed = true;
-            } else if (!shouldBeFixed && isFixed) {
-                nav.css({
-                    position: 'static'
-                });
-                isFixed = false;
-            }
-        });
-    });
+$(document).ready(function() {
+    var nav = $('.nav');
+    var delta_y = nav.offset().top;
+    var is_fixed = false;
+    function scroll() {
+        var should_fix = $(document).scrollTop() > delta_y;
+        if (should_fix && !is_fixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: nav.offset().left,
+                width: nav.width()
+            });
+            is_fixed = true;
+        } else if (!should_fix && is_fixed) {
+            nav.css({
+                position: 'static'
+            });
+            is_fixed = false;
+        }
+    }
+    document.onscroll = scroll;
 });
 
 
