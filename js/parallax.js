c0704b2e844b7e86ff236c0330bc46778882b756
Trey Amador <tmamador@cpp.edu>
2017-03-17 20:49:48 -0700

    server update

--- js/parallax.js
+++ js/parallax.js
@@ -1,53 +1,30 @@
-// scrolls image in background
+/*
+    object which defines parallaxing element
+*/
 
+
+function ParallaxElement(id) {
+    this.element = document.getElementById(id);
+    this.y_init = element.style.top;
+    this.print = function() {
+        console.log(this.element,this.y_init);
+    }
+}
 
 
 $(document).ready(function() {
-    
-    var prlx = $('.parallax-window');
-    var rate = prlx.attr('data-rate');
-    rate = !isNaN(rate) ? rate : 0;
-    // this could be removed
-    rate = 0.7;
-    var init_pos = prlx.offset();
-    
+    var element = '#parallax-element';
+    var prlx = $(element);
+    const y_init = parseInt(prlx.css('top'),10);
+    console.log(y_init);
     function parallax() {
-
-        var curr_pos = init_pos;
-        var scroll = $(document).scrollTop();
-        curr_pos.top = (scroll - init_pos.top)*rate;
-        prlx.offset(curr_pos);
-
-
-
-        /*
-        var curr_scroll = $(document).scrollTop();
-        var scroll_offset = curr_scroll - prev_scroll;
-        var position = prlx.offset();
-        position.top += scroll_offset*rate;
-        prlx.offset(position);
-        prev_scroll = curr_scroll;
-        */
+        prlx = $(element);
+        var y_offset = window.pageYOffset;
+        var y_pos = y_init+(-0.3*(y_offset))+'px';
+        prlx.css('top',y_pos);
     }
-    
-    document.onscroll = parallax;
-
+    window.onscroll = parallax;
 });
 
 
-
-/*
-var velocity = 0.5;
-
-function update() {
-    var y = $(window).scrollTop();
-    $('.container').each(function(){
-        var $element = $(this);
-        var height = $element.height()-18;
-        $(this).css('backgroundPosition','50%',Math.round((height-pos)*velocity)+'px');
-    });
-}
-
-$(window).bind('scroll',update);
-*/
 
