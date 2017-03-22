09e67865337832b8a518cd2e633c04613d4cf7c9
Trey Amador <tmamador@cpp.edu>
2017-03-21 23:21:35 -0700

    updated fade on hover fxn

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,10 +1,28 @@
 
 
+
+
+function hover_fade() {
+    var image = $('.image-git');
+    image.fadeOut(1000,function() {});
+}
+
+
+
+var addEvent = function(object,type,callback) {
+    if (object == null || typeof(object) == 'undefined')
+        return;
+    if (object.addEventListener)
+        object.addEventListener(type,callback,false);
+    else if (object.attachEvent)
+        object.attachEvent('on'+type, callback);
+    else
+        object['on'+type] = callback;
+}
+
+
 $(document).ready(function() {
-    window.onload = function() {
-        var image = $('.image-git');
-        image.fadeOut(1000,function() {});
-    }
+    addEvent(window,'load',hover_fade);
 });
 
 
