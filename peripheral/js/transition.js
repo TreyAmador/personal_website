8a51b5e57b5bf3aa4d90f3db7c8c28648bff8e1f
Trey Amador <tmamador@cpp.edu>
2017-03-22 13:55:35 -0700

    hover functionality

---
+++ peripheral/js/transition.js
@@ -0,0 +1,24 @@
+/*
+    a jQuery transition effect
+*/
+
+/*
+$(document).ready(function() {
+    var placard = $('.transition-entry');
+    placard.click(function() {
+        placard.css('background-color','rgba(0,0,0,1.0)');
+    });
+});
+*/
+
+
+$(document).ready(function() {
+    var placard = $('.transition-entry');
+    placard.hover(function() {
+        placard.css('background-color','rgba(0,0,0,1.0)');
+    },
+    function() {
+        placard.css('background-color','rgba(0,0,0,0.0)');
+    });
+});
+
