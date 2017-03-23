a045b1ab6183e9c80b4ce8fcf47997f9619dbce4
Trey Amador <tmamador@cpp.edu>
2017-03-23 02:58:02 -0700

    modified stylesheets

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,20 +1,5 @@
-
-
-
-
 /*
-function add_hover_fade(addEvent) {
-
-    var image = $('.entry-overlay');
-    function hover_fade_out() {
-        
-        image.fadeOut(1000,function() {});
-    }
-    addEvent(image,'mouseover',hover_fade_out);
-    //window.addEventListener()
-    //'mouseenter';
-    //'mouseover';
-}
+    a jQuery transition effect
 */
 
 
@@ -31,38 +16,169 @@ var addEvent = function(object,type,callback) {
 
 
 
+/*
+
+var slide_fade = function() {
+    var placard = $('.entry-placard'),
+        entry = $('.entry-handle');
+    var entry_height = parseInt(entry.css('height'),10),
+        entry_width = parseInt(entry.css('width'),10),
+        placard_height = parseInt(placard.css('height'),10),
+        placard_width = parseInt(placard.css('width'),10);
+    var delta_y = Math.abs(entry_height-placard_height)/2,
+        delta_x = Math.abs(entry_width-placard_width)/2;
+    var entry_headline = $('.entry-headline');
+    entry.hover(function() {
+        placard.animate({
+            left:'-='+delta_x+'px',
+            top:'-='+delta_y+'px',
+            opacity:'0.8'
+        },250,'linear');
+        entry_headline.animate({
+            left:'+='+delta_x+'px',
+            top:'-='+delta_y+'px',
+            opacity:'1.0'
+        },250,'linear');
+    },
+    function() {
+        placard.animate({
+            left:'+='+delta_x+'px',
+            top:'+='+delta_y+'px',
+            opacity:'0.0'
+        },400,'linear');
+        entry_headline.animate({
+            left:'-='+delta_x+'px',
+            top:'+='+delta_y+'px',
+            opacity:'0.0'
+        },400,'linear');
+    });
+}
+
+*/
 
 
 
-
-$(document).ready(function() {
-    //add_hover_fade(addEvent);
-    
-    
-    //var image = $('.entry-overlay');
-    //image.fadeOut(1000,function() {});
-   
-    //addEvent(window,'load',function() {
-    //    var image = $('.entry-overlay');
-    //    image.fadeOut(1000,function() {});
-    //});
-
-    //add_hover_fade(addEvent);
-
-
-    var overlays = document.getElementsByClassName('entry-overlay');
-    var remind = function() {
-        console.log('reeeeeeee');
+var slide_fade = function() {
+    var entries = $('.entry-handle'),
+        placards = $('.entry-placard'),
+        headlines = $('.entry-headline');
+    for (var i = 0; i < entries.length; ++i) {
+        var entry_height = parseInt(entries.eq(i).css('height'),10),
+            entry_width = parseInt(entries.eq(i).css('width'),10),
+            placard_height = parseInt(placards.eq(i).css('height'),10),
+            placard_width = parseInt(placards.eq(i).css('width'),10);
+        var delta_y = Math.abs(entry_height-placard_height)/2,
+            delta_x = Math.abs(entry_width-placard_width)/2;
+        entries.eq(i).hover(function() {
+            placards.eq(i).animate({
+                left:'-='+delta_x+'px',
+                top:'-='+delta_y+'px',
+                opacity:'0.8'
+            },250,'linear');
+            headlines.eq(i).animate({
+                left:'+='+delta_x+'px',
+                top:'-='+delta_y+'px',
+                opacity:'1.0'
+            },250,'linear');
+        },
+        function() {
+            placards.eq(i).animate({
+                left:'+='+delta_x+'px',
+                top:'+='+delta_y+'px',
+                opacity:'0.0'
+            },400,'linear');
+            headlines.eq(i).animate({
+                left:'-='+delta_x+'px',
+                top:'+='+delta_y+'px',
+                opacity:'0.0'
+            },400,'linear');
+        });
     }
-    for (var i = 0; i < overlays.length; ++i) {
-        overlays[i].addEventListener('click',remind,false);
-    }
 
-    
-    
-});
+    /*
+    var placard = $('.entry-placard'),
+        entry = $('.entry-handle');
+    var entry_height = parseInt(entry.css('height'),10),
+        entry_width = parseInt(entry.css('width'),10),
+        placard_height = parseInt(placard.css('height'),10),
+        placard_width = parseInt(placard.css('width'),10);
+    var delta_y = Math.abs(entry_height-placard_height)/2,
+        delta_x = Math.abs(entry_width-placard_width)/2;
+    var entry_headline = $('.entry-headline');
+    entry.hover(function() {
+        placard.animate({
+            left:'-='+delta_x+'px',
+            top:'-='+delta_y+'px',
+            opacity:'0.8'
+        },250,'linear');
+        entry_headline.animate({
+            left:'+='+delta_x+'px',
+            top:'-='+delta_y+'px',
+            opacity:'1.0'
+        },250,'linear');
+    },
+    function() {
+        placard.animate({
+            left:'+='+delta_x+'px',
+            top:'+='+delta_y+'px',
+            opacity:'0.0'
+        },400,'linear');
+        entry_headline.animate({
+            left:'-='+delta_x+'px',
+            top:'+='+delta_y+'px',
+            opacity:'0.0'
+        },400,'linear');
+    });
+    */
 
 
 
+    /*
+    var placards = $('.entry-placard').each(function(){});
+    var headlines = $('.entry-headline').each(function() {});
+    $('.entry-handle').each(function(i) {
+        var entry_height = parseInt($(this).css('height'),10),
+            entry_width = parseInt($(this).css('width'),10),
+            placard_height = parseInt($(this).css('height'),10),
+            placard_width = parseInt($(this).css('width'),10);
+         var delta_y = Math.abs(entry_height-placard_height)/2,
+            delta_x = Math.abs(entry_width-placard_width)/2;
+        $(this).hover(function() {
+            placards.eq(i).animate({
+                left:'-='+delta_x+'px',
+                top:'-='+delta_y+'px',
+                opacity:'0.8'
+            },250,'linear');
+            headlines.eq(i).animate({
+                left:'+='+delta_x+'px',
+                top:'-='+delta_y+'px',
+                opacity:'1.0'
+            },250,'linear');
+        },
+        function() {
+            placards.eq(i).animate({
+                left:'+='+delta_x+'px',
+                top:'+='+delta_y+'px',
+                opacity:'0.0'
+            },400,'linear');
+            headlines.eq(i).animate({
+                left:'-='+delta_x+'px',
+                top:'+='+delta_y+'px',
+                opacity:'0.0'
+            },400,'linear');
+        });
+    });
+    */
+
+}
+
+
+
+var initialize = function() {
+    slide_fade();
+}
+
+
+$(document).ready(initialize);
 
 
