bb57d40e5a2565922d9874daa3a1695b694d870a
Trey Amador <tmamador@cpp.edu>
2017-03-22 21:10:22 -0700

    addition of transition description

--- peripheral/transition/transition.js
+++ peripheral/transition/transition.js
@@ -3,6 +3,8 @@
 */
 
 
+/*
+
 $(document).ready(function() {
     var placard = $('.entry-placard'),
         entry = $('.entry-image');
@@ -12,17 +14,17 @@ $(document).ready(function() {
         entry_width = parseInt(entry.css('width'),10);    
     var delta_y = Math.abs(entry_height-placard_height)/2,
         delta_x = Math.abs(entry_width-placard_width)/2;
-    
-
     var entry_headline = $('.entry-headline');
-
-    
-
     entry.hover(function() {
         placard.animate({
             left:'-='+delta_x+'px',
             top:'-='+delta_y+'px',
             opacity:'0.8'
+        },250,'linear');
+        entry_headline.animate({
+            left:'+='+delta_x+'px',
+            top:'-='+delta_y+'px',
+            opacity:'1.0'
         },250,'linear');
     },
     function() {
@@ -31,12 +33,69 @@ $(document).ready(function() {
             top:'+='+delta_y+'px',
             opacity:'0.0'
         },400,'linear');
+        entry_headline.animate({
+            left:'-='+delta_x+'px',
+            top:'+='+delta_y+'px',
+            opacity:'0.0'
+        },400,'linear');
     });
-
-
-
-
 });
+
+*/
+
+
+
+
+
+var slide_fade = function() {
+    var placard = $('.entry-placard'),
+        entry = $('.entry-image');
+    var placard_height = parseInt(placard.css('height'),10),
+        placard_width = parseInt(placard.css('width'),10),
+        entry_height = parseInt(entry.css('height'),10),
+        entry_width = parseInt(entry.css('width'),10);    
+    var delta_y = Math.abs(entry_height-placard_height)/2,
+        delta_x = Math.abs(entry_width-placard_width)/2;
+    var entry_headline = $('.entry-headline');
+    entry.hover(function() {
+        placard.animate({
+            left:'-='+delta_x+'px',
+            top:'-='+delta_y+'px',
+            opacity:'0.8'
+        },250,'linear');
+        entry_headline.animate({
+            left:'+='+delta_x+'px',
+            top:'-='+delta_y+'px',
+            opacity:'1.0'
+        },250,'linear');
+    },
+    function() {
+        placard.animate({
+            left:'+='+delta_x+'px',
+            top:'+='+delta_y+'px',
+            opacity:'0.0'
+        },400,'linear');
+        entry_headline.animate({
+            left:'-='+delta_x+'px',
+            top:'+='+delta_y+'px',
+            opacity:'0.0'
+        },400,'linear');
+    });
+}
+
+
+
+var initialize = function() {
+    slide_fade();
+    
+}
+
+
+$(document).ready(initialize);
+
+
+
+
 
 
 
