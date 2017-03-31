86069cb64eb452a88e93b306595c6db3a64604f2
Trey Amador <tmamador@cpp.edu>
2017-03-30 19:17:06 -0700

    added game engine page

--- frontend/controller.js
+++ frontend/controller.js
@@ -15,10 +15,10 @@ var app = angular
                 templateUrl: 'views/about.html',
                 controller: 'aboutCtlr'
             })
-            //.when('/demo', {
-            //    templateUrl: 'views/demo.html',
-            //    controller: 'demoCtlr'
-            //})
+            .when('/cengine', {
+                templateUrl: 'views/democengine.html',
+                controller: 'demoCtlr'
+            })
             .when('/blog', {
                 templateUrl: 'views/blog.html',
                 controller: 'blogCtlr'
@@ -63,63 +63,6 @@ app.controller('aboutCtlr',function($scope,$http) {
     }
     
 });
-
-
-/*
-
-app.controller('resumeCtlr',function($scope,$http) {
-
-    var reset_input = function() {
-        $scope.message.username = '';
-        $scope.message.subject = '';
-        $scope.message.textbody = '';
-        $scope.message.email = '';
-    }
-    var reset_result = function() {
-        $scope.submission_result = '';
-    }
-
-    const docs = {
-        'resume':'files/Amador_Trey_Resume.pdf',
-        'riverside':'files/Amador_Trey_UCR_Unofficial.pdf',
-        'pomona':'files/Amador_Trey_Pomona_Unofficial.pdf',
-        'nih':'https://www.ncbi.nlm.nih.gov/pmc/articles/'+
-                'PMC5013726/pdf/nihms-808778.pdf',
-        'ugrj':'http://ssp.ucr.edu/journal/volumes/'+
-                'volume7/ugrjournal-volvii_amador.pdf'
-    };
-    $scope.open_pdf = function(key) {
-        window.open(docs[key]);
-    }
-
-    $scope.submit = function(message) {
-        var proper_input = true;
-        $('input,textarea').each(function() {
-            var holder = $(this).attr('placeholder');
-            if (holder !== 'E-mail (optional)' && (!$(this) || !$(this).val())) {
-                $scope.submission_result = holder+' space left blank.';
-                proper_input = false;
-            }
-        });
-        if (proper_input) {
-            var user_message = $scope.message;
-            $http.post('/user-message',user_message).then(function(res) {
-                if (res.data.code == 500) {
-                    $scope.submission_result = res.data.errormsg;
-                } else if (res.data.code == 200) {
-                    $scope.submission_result = 'Message sent!';
-                    reset_input();
-                } else {
-                    reset_result();
-                }
-            },function(err) {
-                $scope.submission_result = 'There was a server error.';
-            });
-        }
-    }
-});
-
-*/
 
 
 app.controller('demoCtlr',function($scope) {
