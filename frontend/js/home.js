81c4fef6be0b194f1a3128357eddee697cea4049
Trey Amador <tmamador@cpp.edu>
2017-03-31 03:41:41 -0700

    grid home page

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -35,35 +35,25 @@ var image_scroll = function() {
 
 
 var slide_fade = function() {
-    function delta(entry,placard,dimension) {
-        var entry_dimension = parseInt($(entry).css(dimension),10);
-        var placard_dimension = parseInt(placard.css(dimension),10);
-        return Math.abs(entry_dimension-placard_dimension)/2;
-    }
-    $('.image-handle').hover(function() {
-        var placard = $(this).find('.entry-placard');
-        placard.animate({
-            left:'-='+delta(this,placard,'width')+'px',
-            top:'+='+delta(this,placard,'height')+'px',
-            opacity:'0.8'
-        },300,'linear');
-        $(this).find('.entry-headline').animate({
-            left:'+='+delta(this,placard,'width')+'px',
-            opacity:'1.0'
-        },300,'linear');
-    },function() {
-        var placard = $(this).find('.entry-placard');
-        placard.animate({
-            left:'+='+delta(this,placard,'width')+'px',
-            top:'-='+delta(this,placard,'height')+'px',
+    $('.entry-handle').hover(function() {
+        $(this).find('.entry-placard').animate({
+            opacity:'0.7'
+        },500,'linear');
+        $(this).find('.entry-banner').animate({
+            opacity:'1.0',
+            left:'+=30px'
+        },500,'linear');
+    }, function() {
+        $(this).find('.entry-placard').animate({
             opacity:'0.0'
-        },300,'linear');
-         $(this).find('.entry-headline').animate({
-            left:'-='+delta(this,placard,'width')+'px',
-            opacity:'0.0'
-        },300,'linear');
+        },500,'linear');
+        $(this).find('.entry-banner').animate({
+            opacity:'0.0',
+            left:'-=30px'
+        },500,'linear');
     });
 }
+
 
 
 $(document).ready(function() {
