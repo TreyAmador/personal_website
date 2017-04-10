777d2080b91aa3a3364af8cf2963f3d1d8e93fc1
Trey Amador <tmamador@cpp.edu>
2017-04-10 09:01:10 -0700

    simplified express site

---
+++ peripheral/personal_express/backend/api.js
@@ -0,0 +1,14 @@
+
+
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+
+router.get('/',function(req,res,next) {
+    res.send('howdy');
+});
+
+
+
+module.exports = router;
