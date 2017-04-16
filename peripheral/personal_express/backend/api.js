400cb00616c4cb529f67d0c036a09747735e3126
Trey Amador <tmamador@cpp.edu>
2017-04-15 23:54:43 -0700

    site style improved

--- peripheral/personal_express/backend/api.js
+++ peripheral/personal_express/backend/api.js
@@ -11,29 +11,29 @@ var path = require('path');
 //var nodemailer = require('nodemailer');
 
 app.get('/',function(req,res,next) {
-    res.render('../frontend/views/home.ejs')
+    res.render('../frontend/views/home')
 });
 
 
 app.get('/about',function(req,res,next) {
-    res.render('../frontend/views/about.ejs');
+    res.render('../frontend/views/about');
 });
 
 
 app.get('/contact',function(req,res,next) {
-    res.render('../frontend/views/contact.ejs');
+    res.render('../frontend/views/contact');
 });
 
 
 app.get('/blog',function(req,res,next) {
-    res.render('../frontend/views/blog.ejs',{
+    res.render('../frontend/views/blog',{
         constructor:'This page is construction...'
     });
 });
 
 
 app.get('/cengine',function(req,res,next) {
-    res.render('../frontend/views/demos/cengine.ejs');
+    res.render('../frontend/views/demos/cengine');
 });
 
 
