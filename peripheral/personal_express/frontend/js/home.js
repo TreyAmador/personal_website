45cfd745dc62ecb6647c31a58119ff48c82edfa1
Trey Amador <tmamador@cpp.edu>
2017-04-16 02:13:58 -0700

    footer added

--- peripheral/personal_express/frontend/js/home.js
+++ peripheral/personal_express/frontend/js/home.js
@@ -105,11 +105,35 @@ var underline_hover = function() {
 }
 
 
+// expand contact panel if at bottom
+var contact_expand = function() {
+    var expanded = false;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+            $('.home-contact').animate({
+                left:'3%',
+                width:'94%',
+                height:'92%'
+            });
+            expanded = true;
+        } else if (expanded === true) {
+            $('.home-contact').animate({
+                left:'0',
+                width:'100%',
+                height:'100%'
+            },500);
+            expanded = false;
+        }
+    });
+}
+
+
 $(document).ready(function() {
     anchor_listener();
     project_hover();
     //underline_hover();
     //demo_popup();
+    contact_expand();
 });
 
 
