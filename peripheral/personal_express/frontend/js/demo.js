d170147804bab7bd2a845f8be6a6573532211788
Trey Amador <tmamador@cpp.edu>
2017-04-17 08:56:56 -0700

    updated demo view

--- peripheral/personal_express/frontend/js/demo.js
+++ peripheral/personal_express/frontend/js/demo.js
@@ -3,8 +3,33 @@
 */
 
 
+var demo_toggle = function() {
+
+    $('.left-demo').hover(function() {
+        $(this).animate({
+            left:'-=20px'
+        },300);
+    }, function() {
+        $(this).animate({
+            left:'+=20px'
+        },200);
+    });
+
+    $('.right-demo').hover(function() {
+        $(this).animate({
+            left:'+=20px'
+        },300);
+    }, function() {
+        $(this).animate({
+            left:'-=20px'
+        },200);
+    });
+    
+}
+
 
 $(document).ready(function() {
     //load_directory();
+    demo_toggle();
 });
 
