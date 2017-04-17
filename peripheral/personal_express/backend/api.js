8085497991737c4f677245ced6b05fe688756e40
Trey Amador <tmamador@cpp.edu>
2017-04-16 23:54:02 -0700

    demo router updated

--- peripheral/personal_express/backend/api.js
+++ peripheral/personal_express/backend/api.js
@@ -3,12 +3,10 @@
 */
 
 
-
 var express = require('express');
 var app = express.Router();
 var path = require('path');
 
-//var nodemailer = require('nodemailer');
 
 app.get('/',function(req,res,next) {
     res.render('../frontend/views/home')
@@ -25,21 +23,64 @@ app.get('/contact',function(req,res,next) {
 });
 
 
+app.post('/contact',function(req,res,next) {    
+    res.send('Message sent!');
+});
+
+
+var demo_factory = function(routing) {
+
+    var Demo = function(media,header,textbody) {
+        this.media = media;
+        this.header = header;
+        this.textbody = textbody;
+    }
+
+    var Demos = {
+        '/cengine': new Demo(
+            '<video controls><source src="../media/cengine.mp4" type="video/mp4"></video>',
+            'C++ game engine with SDL graphics library',
+            ''
+        ),
+        '/trader': new Demo(
+            'trader video',
+            '',
+            ''
+        ),
+        '/robot': new Demo(
+            '',
+            '',
+            ''
+        ),
+        '/drum': new Demo(
+            '',
+            '',
+            ''
+        ),
+        '/asteroids': new Demo(
+            '',
+            '',
+            ''
+        )
+    }
+
+    return Demos[routing];
+}
+
+
+app.get('/cengine',function(req,res,next) {
+    var demo = demo_factory('/cengine');
+
+    res.render('../frontend/views/demo');
+
+});
+
+
 app.get('/blog',function(req,res,next) {
     res.render('../frontend/views/blog',{
         constructor:'This page is construction...'
     });
 });
-
-
-app.get('/cengine',function(req,res,next) {
-    res.render('../frontend/views/demos/cengine');
-});
-
-
-//app.get('/error',function(req,res,next) {
-//    res.render('../frontend/views/error.ejs');
-//});
 
 
 module.exports = app;
--- peripheral/personal_express/frontend/controller.js
+++ peripheral/personal_express/frontend/controller.js
@@ -6,8 +6,39 @@
 var app = angular.module('personal',[]);
 
 
-app.controller('homeCtrl',function($scope,$http) {
-    //console.log('your are in the home page!');
+app.config(function($provide) {
+    $provide.factory('demo',function() {
+        return {
+            cengine:'cengine',
+            trader:'trader',
+            robot:'robot',
+            drum:'drum',
+            asteroids:'asteroids'
+        };
+    });
+});
+
+
+app.controller('homeCtrl',function($scope,$rootScope,$http,demo) {
+    // home controller
+    
+    $scope.query_demo = function(route) {
+        //console.log(demo.cengine);
+
+        $rootScope.demo = route;
+
+    }
+
+});
+
+
+app.controller('demoCtrl',function($scope,$rootScope,$http,demo) {
+    // projects slide back and forth
+
+    //console.log('from demo controller',demo.cengine);
+    console.log($rootScope.demo);
+
+
 });
 
 
@@ -40,7 +71,7 @@ app.controller('formCtrl',function($scope,$http) {
                 url:'/contact',
                 data:$scope.message
             }).then(function(res) {
-                $scope.submission_result = 'Message sent.';
+                $scope.submission_result = 'Message sent!';
                 reset_input();
             }, function(res) {
                 $scope.submission_result = 'Error sending message!';
@@ -55,8 +86,6 @@ app.controller('docCtrl',function($scope,$http) {
 
     const docs = {
         'resume':'files/Amador_Trey_Web_Resume.pdf',
-        'riverside':'files/Amador_Trey_UCR_Unofficial.pdf',
-        'pomona':'files/Amador_Trey_Pomona_Unofficial.pdf',
         'nih':'https://www.ncbi.nlm.nih.gov/pmc/articles/'+
                 'PMC5013726/pdf/nihms-808778.pdf',
         'ugrj':'http://ssp.ucr.edu/journal/volumes/'+
