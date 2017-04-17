b72fa5a84a0a46dcb3b0f1e42700ceaa26d08700
Trey Amador <tmamador@cpp.edu>
2017-04-17 13:11:40 -0700

    udpated video player for demos

--- peripheral/personal_express/frontend/js/demo.js
+++ peripheral/personal_express/frontend/js/demo.js
@@ -28,8 +28,28 @@ var demo_toggle = function() {
 }
 
 
+var toggle_video = function() {
+
+    var video = document.getElementById('demo-video');
+    var playing = false;
+    $('.demo-media').click(function() {
+        if (playing) {
+            video.pause();
+            playing = false;
+        } else {
+            video.play();
+            playing = true;
+        }
+        
+    });
+    
+
+}
+
+
 $(document).ready(function() {
     //load_directory();
     demo_toggle();
+    toggle_video();
 });
 
