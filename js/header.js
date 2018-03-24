703783771768ceab59189b51f9a2f5908b35c550
TreyAmador <tmamador@cpp.edu>
2018-03-23 18:13:09 -0700

    update header bar

--- js/header.js
+++ js/header.js
@@ -2,10 +2,14 @@
 
 
 
+function loadMeta() {
+    $('head').load('../views/meta.html');
+}
+
+
 function loadHeader() {
     $('.navbar-insert').load('../views/header.html');
 }
-
 
 
 $(document).ready(function() {
